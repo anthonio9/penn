2f9de960269439469a172675a9eca79b0eeb01cf
Jankowski Antoni <jankowa1@bach.org.aalto.fi>
2023-12-21 00:31:09 +0200

    fcnf0++-gset-all-voice: New config for all voices

---
+++ config/fcnf0++-gset-voiced.py
@@ -0,0 +1,18 @@
+MODULE = 'penn'
+
+# Configuration name
+CONFIG = 'fcnf0++-gset-voiced'
+
+# gset only
+DATASETS = ['gset']
+
+LOG_INTERVAL = 500
+
+# audio parameters
+SAMPLE_RATE = 11025
+
+# the original hopsize is 256 samples, this is 4 times less than that
+HOPSIZE = 64 
+
+# use only the voiced frames
+VOICED_ONLY = True
