c4f569bad46eeeefac7c0419103eddf78736ffdb
antoni <antoni.s.jankowski@gmail.com>
2024-01-25 00:33:55 +0200

    New weight + drop configurations

---
+++ config/ppn-split-weight-drop8.py
@@ -0,0 +1,33 @@
+MODULE = 'penn'
+
+# Configuration name
+CONFIG = 'ppn-split-weight-drop8'
+
+# gset only
+DATASETS = ['gset']
+
+EVALUATION_DATASETS = ['gset']
+
+LOG_INTERVAL = 500
+
+# audio parameters
+SAMPLE_RATE = 11025
+
+# the original hopsize is 256 samples, this is 4 times less than that
+HOPSIZE = 64 
+
+# use only the voiced frames
+VOICED_ONLY = True
+
+STRING_INDEX = None
+
+# poly pitch net model
+MODEL = 'ppn'
+
+PITCH_CATS = 6
+
+GSET_SPLIT_PLAYERS = True
+
+WEIGHT_DECAY = 1e-2
+
+DROPOUT = 0.5
