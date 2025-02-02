529544eaf7f7d066ea90d31861c246bb78271b7d
antoni <antoni.s.jankowski@gmail.com>
2025-02-02 11:51:04 +0100

    WIP: remove PolyPENNFCNBase

--- penn/model/poly_pitch_net.py
+++ penn/model/poly_pitch_net.py
@@ -7,6 +7,7 @@ class PolyPitchNet(torch.nn.Sequential):
         super().__init__(*layers)
 
     def forward(self, frames : torch.Tensor):
+        breakpoint()
         # shape=(batch, 1, penn.WINDOW_SIZE) =>
         # shape=(batch, penn.PITCH_BINS, penn.NUM_TRAINING_FRAMES)
         logits = super().forward(frames[:, :, 16:-15])
@@ -17,31 +18,6 @@ class PolyPitchNet(torch.nn.Sequential):
         # shape [128, 6, 1440, 1]
         logits = torch.stack(logits_chunks, dim=1)
 
-        logits_dict = { penn.model.KEY_LOGITS   : logits }
-        return logits_dict
-
-
-class PolyPENNFCNBase(PolyPitchNet):
-    def forward(self, frames : torch.Tensor):
-        frames = frames.squeeze(dim=1)
-
-        frames_list = torch.chunk(frames, chunks=frames.shape[-1] // penn.HOPSIZE, dim=-1)
-        # frames shape [BATCH_SIZE, FRAMES, FRAME_LENGTH]
-        # FRAMES dimention is made out of initial frames and the actual frames which are present in the ground truth. Meaning that the first few frames made out of penn.WINDOW_SIZE do not translate into ground truth at all and should be randomize on the ground truth side.
-        frames = torch.stack(frames_list, dim=1)
-        # [BS, T, HOPSIZE] => [BS, HOPSIZE, T]
-        frames = torch.permute(frames, (0, 2, 1))
-
-        logits = torch.nn.Sequential.forward(self, frames)
-
-        # [128, 8640, *] => [128, 1440, *] * 6
-        logits_chunks = logits.chunk(penn.PITCH_CATS, dim=-2)
-
-        # shape [128, 6, 1440, *]
-        logits = torch.stack(logits_chunks, dim=1)
-
-
-        # [BS, PITCH_CATS, PITCH_BINS, T] => [BS, PITCH_CATS, T, PITCH_BINS]
         logits_dict = { penn.model.KEY_LOGITS   : logits }
         return logits_dict
 
